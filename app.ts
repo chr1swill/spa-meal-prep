899e89f1ab3564ee2124a23c6a3ce521c291792c
Chris <127428764+chr1swill@users.noreply.github.com>
2023-10-23 01:53:58 +0000

    feat - add html selector to app.ts

---
+++ app.ts
@@ -0,0 +1,30 @@
+// PAGES
+const pageSelectMeat = document.querySelector("#pageSelectMeat") as HTMLElement
+const selectTopSirlion = document.querySelector('[data-button-select="top-sirlion"]') as HTMLButtonElement
+const selectChickenBreast = document.querySelector('[data-button-select="chicken-breast"]') as HTMLButtonElement
+const selectOther = document.querySelector('[data-button-select="other"]') as HTMLButtonElement
+const confirmMeatPage = document.querySelector('[data-button-confirm="pageMeat"]') as HTMLButtonElement
+
+const pageSelectQuanity = document.querySelector("#pageSelectQuanity") as HTMLElement
+const inputMeatQuanity = document.querySelector("#inputMeatQuanity") as HTMLInputElement
+const confirmQuanityPage = document.querySelector('[data-button-confirm=pageQuanity]') as HTMLButtonElement
+const backBtnOnQuanity = document.querySelector('[data-back-button="pageQuanity"]') as HTMLButtonElement
+
+const pageSelectDays = document.querySelector("#pageSelectDays") as HTMLElement
+const confirmDaysPage = document.querySelector('[data-button-confirm="pageDays"]') as HTMLButtonElement
+const backBtnOnDays = document.querySelector('[data-back-button="pageDays"]') as HTMLButtonElement
+
+const pageTotalResult = document.querySelector("#pageTotalResult") as HTMLElement
+const resultTotalMeat = document.querySelector("#resultTotalMeat") as HTMLElement
+const resultTotalDays = document.querySelector("#resultTotalDays") as HTMLElement
+const resultProtienDaily = document.querySelector("#resultProtienDaily") as HTMLElement
+const resultMeatDaily = document.querySelector("#resultMeatDaily") as HTMLElement
+const confirmResultPage = document.querySelector('[data-button-confirm="pageResult"]') as HTMLButtonElement
+const backBtnOnResult = document.querySelector('[data-back-button="pageResult"]') as HTMLButtonElement
+
+// selector for the incrementor 
+const incrementerInput = document.querySelector('[data-input-incrementer]') as HTMLInputElement
+const incrementerBtnMinus = document.querySelector('[data-incrementer-left]') as HTMLButtonElement
+const incrementerBtnPlus = document.querySelector('[data-incrementer-right]') as HTMLButtonElement
+
+
